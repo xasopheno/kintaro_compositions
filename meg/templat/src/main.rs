11811e4e3afc81cc77567cfc64cdcb4218b0c56c
xasopheno <danny@xasopheno.com>
2022-01-03 12:06:11 -0500

    panning in place

--- meg/templat/src/main.rs
+++ meg/templat/src/main.rs
@@ -1,10 +1,11 @@
 #[allow(unused_imports)]
 use log::{debug, error, info, log, log_enabled, Level};
-use serde::Serialize;
+use num_rational::Rational64;
+use serde::{Deserialize, Serialize};
+use serde_json::json;
 use serde_json::value::{Map, Value as Json};
 use std::collections::HashMap;
 use std::path::Path;
-use tap::prelude::*;
 use walkdir::WalkDir;
 
 use std::error::Error;
@@ -16,7 +17,7 @@ use handlebars::{to_json, Handlebars};
 pub struct Meg {
     filename: String,
     tag: String,
-    pan: f32,
+    pan: String,
     scale: String,
 }
 const TARGET: &str = "0";
@@ -50,7 +51,12 @@ fn get_filenames(target: &str) -> Vec<String> {
         .collect()
 }
 
-type Coords = HashMap<String, Vec<String>>;
+#[derive(Deserialize)]
+struct CoordData {
+    sensors: Vec<String>,
+    pan: Rational64,
+}
+type Coords = HashMap<String, CoordData>;
 
 fn get_coordinates() -> Coords {
     let file = std::fs::File::open("./neuromeg.json").unwrap();
@@ -65,16 +71,10 @@ pub fn make_data() -> Map<String, Json> {
 
     let mut data = Map::new();
 
-    // let megs = vec![Meg {
-    // filename: "./data/wi1lmhbs/meg0111_0.csv".to_string(),
-    // pan: -1.0,
-    // scale: SCALE.to_string(),
-    // }];
-
     let mut megs: Vec<Meg> = filenames
         .iter()
         .map(|filename| {
-            let mut filestem = Path::new(filename)
+            let mut file_stem = Path::new(filename)
                 .file_stem()
                 .unwrap()
                 .to_string_lossy()
@@ -83,30 +83,37 @@ pub fn make_data() -> Map<String, Json> {
                 .collect::<Vec<&str>>()[0]
                 .to_owned();
 
-            filestem.retain(|s| !r#"meg"#.contains(s));
+            file_stem.retain(|s| !r#"meg"#.contains(s));
 
-            let tag = get_tag(&filestem, &coords).expect("no location found");
+            let channel_context = get_channel_context(&file_stem, &coords).unwrap();
             Meg {
                 filename: filename.to_string(),
-                pan: 0.0,
-                tag,
+                pan: channel_context.pan,
+                tag: channel_context.brain_region,
                 scale: SCALE.into(),
             }
         })
         .collect();
 
-    // megs.sort();
-    // panic!();
+    megs.sort_by_key(|m| m.filename.clone());
 
     data.insert("megs".to_string(), to_json(&megs));
     data
 }
 
-fn get_tag(filestem: &str, coords: &Coords) -> Option<String> {
-    for (key, values) in coords.into_iter() {
-        for value in values {
-            if filestem.starts_with(value) {
-                return Some(key.clone());
+struct ChannelContext {
+    brain_region: String,
+    pan: String,
+}
+
+fn get_channel_context(filestem: &str, coords: &Coords) -> Option<ChannelContext> {
+    for (brain_region, values) in coords.into_iter() {
+        for sensor in &values.sensors {
+            if filestem.starts_with(sensor) {
+                return Some(ChannelContext {
+                    brain_region: brain_region.clone(),
+                    pan: format!("{}/{}", values.pan.numer(), values.pan.denom()),
+                });
             }
         }
     }
