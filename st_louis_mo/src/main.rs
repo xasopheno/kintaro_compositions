96e0fac8471949597e1f716a6b2b7e20c21dc8b2
xasopheno <danny@xasopheno.com>
2022-05-13 11:32:34 -0400

    family is home works

--- st_louis_mo/src/main.rs
+++ st_louis_mo/src/main.rs
@@ -1,6 +1,6 @@
 mod color;
 mod instancer;
-use kintaro::config::{named_colorsets, FramePass};
+use kintaro::config::FramePass;
 use kintaro::error::KintaroError;
 use kintaro::renderable::EventStreamConfig;
 use kintaro::renderable::GlyphyConfig;
@@ -19,42 +19,31 @@ fn main() -> Result<(), KintaroError> {
 }
 
 fn frame_passes() -> Vec<FramePass> {
-    vec![
-        FramePass {
-            output_frame: "main",
-            renderables: vec![
-                RenderableConfig::Toy(ToyConfig {
-                    shader_path: "src/toy.wgsl",
-                }),
-                RenderableConfig::EventStreams(EventStreamConfig {
-                    socool_path: "./src/template.socool".to_string(),
-                    shader_path: "./src/shader.wgsl",
-                }),
-                RenderableConfig::Glyphy(GlyphyConfig::GlypyTextConfig {
-                    text: vec![("St. Louis, MO", "#321145")],
-                    location: (0.7, 0.9),
-                    scale: 100.0,
-                }),
-            ],
-        },
-        // FramePass {
-        // output_frame: "main",
-        // renderables: vec![
-        // // RenderableConfig::ImageRenderer(ImageRendererConfig {
-        // // image_path: "src/image_renderer/milo.png",
-        // // }),
-        // RenderableConfig::Origami(OrigamiConfig {
-        // shader_path: "./src/origami_shader.wgsl",
-        // n_indices: 30,
-        // n_vertices: 20,
-        // }),
-        // RenderableConfig::Sampler(SamplerConfig {
-        // shader_path: "./src/sampler/sampler_shader.wgsl",
-        // input_frame: "frame1",
-        // }),
-        // ],
-        // },
-    ]
+    vec![FramePass {
+        output_frame: "main",
+        renderables: vec![
+            RenderableConfig::Toy(ToyConfig {
+                shader_path: "src/toy.wgsl",
+            }),
+            RenderableConfig::EventStreams(EventStreamConfig {
+                socool_path: "./src/template.socool".to_string(),
+                shader_path: "./src/shader.wgsl",
+                instancer: Box::new(MegInstancer {}),
+                shape: Shape {
+                    n_vertices: 30,
+                    n_indices: 30,
+                    position: Box::new(RandPosition),
+                    color: Box::new(color::color_map()),
+                    indices: Box::new(RandIndex),
+                },
+            }),
+            RenderableConfig::Glyphy(GlyphyConfig::GlypyTextConfig {
+                text: vec![("St. Louis, MO", "#321145")],
+                location: (0.7, 0.9),
+                scale: 50.0,
+            }),
+        ],
+    }]
 }
 
 pub fn make_config<'a>() -> Config<'a> {
@@ -68,20 +57,11 @@ pub fn make_config<'a>() -> Config<'a> {
     };
     let (cameras, instance_mul) = Config::handle_save(instance_mul);
     Config {
-        renderable_configs: frame_passes(),
+        frame_passes: frame_passes(),
         composition_name: "St. Louis, MO",
-        instancer: Box::new(MegInstancer {}),
         instance_mul,
-        accumulation: false,
         volume: 0.20,
         window_size: (1920 * 2, 1080 * 2),
         cameras,
-        shape: Shape {
-            n_vertices: 30,
-            n_indices: 30,
-            position: Box::new(RandPosition),
-            color: Box::new(color::color_map()),
-            indices: Box::new(RandIndex),
-        },
     }
 }
