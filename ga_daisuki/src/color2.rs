9b5c356dd7131de87649853ff072bc986ad0b8fd
xasopheno <danny@xasopheno.com>
2022-06-07 23:09:34 -0400

    ga daiski

--- ga_daisuki/src/color2.rs
+++ ga_daisuki/src/color2.rs
@@ -6,7 +6,12 @@ use kintaro::{colorsets_from_vec_hex_strings, ColorMap, ColorSets};
 pub fn named_colorsets<'a>() -> Vec<(&'a str, Vec<&'a str>)> {
     vec![
         ("a", vec!["#001122", "#110022"]),
-        ("a", vec!["#331122", "#113322"]),
+        ("b", vec!["#331122", "#113322"]),
+        ("c", vec!["#331122", "#113322"]),
+        ("d", vec!["#baba33", "#fafa22"]),
+        ("e", vec!["#3333ab", "#ad5522"]),
+        ("f", vec!["#883333", "#885554"]),
+        ("g", vec!["#2233bb", "#3355dd"]),
     ]
 }
 
