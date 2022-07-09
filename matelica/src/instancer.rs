0f0791ad8fcc59cca48f7f07d19aaf1ac02c8f23
xasopheno <danny@xasopheno.com>
2022-07-09 22:49:13 +0200

    wow

---
+++ matelica/src/instancer.rs
@@ -0,0 +1,37 @@
+use cgmath::Rotation3;
+use kintaro::{Instance, Instancer, InstancerInput, InstancerOutput};
+use rand::Rng;
+
+#[derive(Debug, Clone)]
+pub struct Instancer1 {}
+
+impl Instancer for Instancer1 {
+    fn update_instance(&self, instance: &mut Instance, dt: f32) {
+        let mut rng = rand::thread_rng();
+        instance.life -= dt * 0.1;
+        instance.position.x += 10.0 * (2.0 - instance.life) * f32::signum(instance.position.x);
+        // instance.position.x += rng.gen_range(-10.0..10.0);
+        instance.position.y += 30.0 * (2.0 - instance.life) * f32::signum(instance.position.y);
+        instance.position.y +=
+            f32::cos(dt * 1000.0 * f32::sin(instance.position.z) * f32::tan(instance.life));
+        instance.position.z -= 1.0 * (2.0 - instance.life);
+    }
+
+    fn op4d_to_instance_transformation(&self, input: InstancerInput) -> InstancerOutput {
+        let mut rng = rand::thread_rng();
+        let rotation = cgmath::Quaternion::from_axis_angle(
+            cgmath::Vector3::unit_x(),
+            cgmath::Deg(rng.gen_range(-10.9..10.9)),
+        );
+
+        InstancerOutput {
+            x: -input.x,
+            y: input.y,
+            z: input.z,
+            length: input.length,
+            life: input.life,
+            size: input.size * 5.0,
+            rotation,
+        }
+    }
+}
