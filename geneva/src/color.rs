8890c9b1cffef7aca3724bd642877f22f59a43ad
Danny Meyer <danny@xasopheno.com>
2022-12-31 14:23:54 -0800

    cool dragon thing

--- geneva/src/color.rs
+++ geneva/src/color.rs
@@ -5,16 +5,16 @@ use kintaro::{colorsets_from_vec_hex_strings, ColorMap, ColorSets};
 
 pub fn named_colorsets<'a>() -> Vec<(&'a str, Vec<&'a str>)> {
     vec![
-        ("sendMessageMutation", vec!["#dd1133", "#030303"]),
-        // ("b", vec!["#293923", "#303030"]),
-        // ("c", vec!["#744253", "#ccddaa"]),
-        ("createRoom", vec!["#447880", "#facba2"]),
-        ("acceptFriendRequest", vec!["#63474D", "#adc37a"]),
-        ("createHome", vec!["#323341", "#2a0d10"]),
-        // ("g", vec!["#1A3c3A", "#fadf23"]),
-        // ("h", vec!["#aa4411", "#4e3636"]),
-        // ("i", vec!["#aa2244", "#232423"]),
-        // ("j", vec!["#aa3300", "#3f2527"]),
+        ("a", vec!["#dd1133", "#030303"]),
+        ("b", vec!["#293923", "#303030"]),
+        ("c", vec!["#744253", "#ccddaa"]),
+        ("d", vec!["#447880", "#facba2"]),
+        ("e", vec!["#63474D", "#adc37a"]),
+        ("f", vec!["#323341", "#2a0d10"]),
+        ("g", vec!["#1A3c3A", "#fadf23"]),
+        ("h", vec!["#aa4411", "#4e3636"]),
+        ("i", vec!["#aa2244", "#232423"]),
+        ("j", vec!["#aa3300", "#3f2527"]),
     ]
 }
 
