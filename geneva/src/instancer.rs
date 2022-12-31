eca8b7006ccfed25706ffa98edca5d07603c92f4
Danny Meyer <danny@xasopheno.com>
2022-12-31 14:58:35 -0800

    dragon and geneva

--- geneva/src/instancer.rs
+++ geneva/src/instancer.rs
@@ -9,11 +9,9 @@ impl Instancer for Instancer1 {
     fn update_instance(&self, instance: &mut Instance, dt: f32) {
         let mut rng = rand::thread_rng();
         instance.life -= dt * 0.1;
-        instance.position.x += f32::sin(instance.life * 3.0) * 1000.0 * (2.0 - instance.life);
-        // * f32::signum(instance.position.x);
+        instance.position.x += 10.0 * (2.0 - instance.life) * f32::signum(instance.position.x);
         // instance.position.x += rng.gen_range(-10.0..10.0);
-        instance.position.y -= f32::sin(instance.life * 5.0) * -1000.0 * (2.0 - instance.life);
-        // * f32::signum(instance.position.y);
+        instance.position.y += 30.0 * (2.0 - instance.life) * f32::signum(instance.position.y);
         instance.position.y +=
             f32::cos(dt * 1000.0 * f32::sin(instance.position.z) * f32::tan(instance.life));
         instance.position.z -= 1.0 * (2.0 - instance.life);
